6a50f742d8617d80d0a2ba747e7bd450743d85da
Tomas Bubenicek <tombuben@gmail.com>
2020-11-18 17:53:27 +0100

    Single cube rendering

---
+++ Assets/Scripts/CubeFaces.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFaces
+{
+    public static readonly Vector3[] Vertices = {
+        //front
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f),
+        //back
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+    };
+
+    public static readonly int[,] FaceIndices = {
+        // front
+        {0, 1, 2,
+        2, 3, 0},
+        // right
+        {1, 5, 6,
+        6, 2, 1},
+        // back
+        {5, 4, 7,
+         7, 6, 5},
+        // left
+        {4, 0, 3,
+        3, 7, 4},
+        // bottom
+        {4, 5, 1,
+        1, 0, 4},
+        // top
+        {3, 2, 6,
+        6, 7, 3}
+    };
+
+    public static readonly Vector2[] UVs =
+    {
+        new Vector2(1.0f, 0.0f),
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+    };
+}
---
+++ Assets/Scripts/VoxelChunk.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
+public class VoxelChunk : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    private MeshFilter _meshFilter;
+    
+    private const uint ChunkSize = 16;
+    private sbyte[] _voxels = new sbyte[ChunkSize * ChunkSize * ChunkSize];
+
+    private void Awake()
+    {
+        if (!SystemInfo.supportsComputeShaders)
+        {
+            Debug.LogError("No support for compute shaders found!");
+            Destroy(this);
+            return;
+        }
+        
+        //Init array
+        Array.Clear(_voxels, 0, _voxels.Length);
+        _voxels[0] = 1;
+        
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshFilter.mesh = CreateMesh();
+    }
+
+    private Mesh CreateMesh()
+    {
+        var vertices = new List<Vector3>();
+        var indices = new List<int>();
+        var uvs = new List<Vector2>();
+        var maxIndex = 0;
+        
+        
+        for (int face = 0; face < 6; face++)
+            for (int i = 0; i < 6; i++)
+            {
+                int index = CubeFaces.FaceIndices[face, i];
+                vertices.Add(CubeFaces.Vertices[index]);
+                uvs.Add(CubeFaces.UVs[i]);
+                indices.Add(maxIndex++);
+            }
+        
+        var mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = indices.ToArray();
+        mesh.uv = uvs.ToArray();
+        
+        mesh.RecalculateNormals();
+        
+        return mesh;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
